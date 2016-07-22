a7874fc8aa1078584e95c7a11e81b90313be3b7a
emilyh <emily.lhumble@gmail.com>
2016-07-22 11:11:42 +0200

    data file

--- R/data.R
+++ R/data.R
@@ -4,3 +4,11 @@
 #'
 #' @source \url{http://www.sciencedirect.com/science/article/pii/S0960982201004481}
 "u.americanus_mc1r_primers"
+
+
+#' Canis lupus familiaris melanocortin 1 receptor (alpha melanocyte stimulating hormone receptor) (MC1R), mRNA
+#'
+#' @format A  list of 1 vector of chars, class: SeqFastadna
+#'
+#' @source \url{http://www.ncbi.nlm.nih.gov/nuccore/NM_001014282.2}
+"c.lupus_mc1r_seq"
--- R/seq.R
+++
@@ -1,6 +0,0 @@
-#' Canis lupus familiaris melanocortin 1 receptor (alpha melanocyte stimulating hormone receptor) (MC1R), mRNA
-#'
-#' @format A  list of 1 vector of chars, class: SeqFastadna
-#'
-#' @source \url{http://www.ncbi.nlm.nih.gov/nuccore/NM_001014282.2}
-"c.lupus_mc1r_seq"
