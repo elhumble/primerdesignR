8fae120f8a294f440508072118befd11b5b95e98
emilyh <emily.lhumble@gmail.com>
2016-07-22 10:44:35 +0200

    datasets added

---
+++ R/primers.R
@@ -0,0 +1,6 @@
+#' Primer sequences used by Ritland et al. (2001) to sequence the MC1R in the Kermode Bear
+#'
+#' @format A  list of 1 vector of chars, class: SeqFastadna
+#'
+#' @source \url{http://www.sciencedirect.com/science/article/pii/S0960982201004481}
+"u.americanus_mc1r_primers"
