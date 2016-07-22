8961dd1cedf7645c8ac8afb735e72b7ef7c6a026
emilyh <emily.lhumble@gmail.com>
2016-07-22 14:18:27 +0200

    run_primer3 function and DOC updated

--- R/run_primer3.R
+++ R/run_primer3.R
@@ -1,10 +1,10 @@
 #' run_primer3: Run primer3_core executable
-#' @param input Specify the name of your primer3 input file.
-#' @param primer3path Specify the path of where the primer3_core executable is stored.
 #' @param indir Specify the path of the directory in which the input file is stored.
+#' @param input Specify the name of your primer3 input file.
+#' @param primer3_corepath Specify the path of where the primer3_core executable is stored.
 #' @param outdir Specify the path of the directory in which the output file should be written.
 #' @export
 
-run_primer3 <- function(input, primer3path, indir, outdir){
-        system(paste0(primer3path," <",indir,input,"> ",outdir,input,"_OUT"))
+run_primer3 <- function(indir, input, primer3_corepath, outdir){
+        system(paste0(primer3_corepath," <",indir,input,"> ",outdir,input,"_OUT"))
 }
